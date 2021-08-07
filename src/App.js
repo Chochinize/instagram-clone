0090c2922b1a2968a1af3c579c65be11e9c882ae
chochiniz <chochinize@gmail.com>
2021-08-07 15:47:50 +0200

    style some stuffs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
-import './App.css';
+import Main from './Components/MainPage'
+import {useReducer} from 'react'
+import './tailwind.css'
+const reducer = (state,action)=>{
+  return state+action;
+}
 
 function App() {
+const[value,dispatch] = useReducer(reducer,0);
+
   return (
-    <div className="App">
-     <h1>MY REACT APP</h1>
+    <div className='bg-red-100'>
+      <Main />
+     <h2 onClick={()=>dispatch(1)}>+</h2>
+     <h2 onClick={()=>dispatch(-1)}>-</h2>
+      
+     <h2>{value}</h2>
      <h2>SECOND</h2>
     </div>
   );
---
+++ src/Components/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainPage = () => {
+    return (
+        <div>
+            <h1 className='bg-red-200'>HELLO</h1>
+        </div>
+    )
+}
+
+export default MainPage
